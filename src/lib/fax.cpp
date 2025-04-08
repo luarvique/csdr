de2d560346d2982b0cd07473ec0aa06abe529503
Marat Fayzullin <luarvique@gmail.com>
2025-04-07 21:27:54 -0400

    Now using "END-PAGE!" mark made of 3x3 bytes.

--- src/lib/fax.cpp
+++ src/lib/fax.cpp
@@ -648,21 +648,18 @@ void FaxDecoder<T>::printBmpLine()
 template <typename T>
 unsigned int FaxDecoder<T>::printBmpEmptyLines(unsigned int lines)
 {
-    static const unsigned char endMark[] =
-    {
-        // E N D - 0x45 0x4E 0x44 - 0100 0101 0100 1110 0100 0100
-        0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0x00,
-        0xFF, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF,
-        0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF
-    };
+    // 3x3 = 9 bytes
+    static const char endMark[] = { 'E', 'N', 'D', '-', 'P', 'A', 'G', 'E', '!' };
 
     unsigned int size = lineWidth * colors;
     unsigned int todo = this->writer->writeable() / size;
     char buf[size];
 
+    // Clear line to white
+    memset(buf, 0xFF, size);
+
     // Insert end-mark at the start of each empty line
     memcpy(buf, endMark, sizeof(endMark));
-    memset(buf + sizeof(endMark), 0xFF, size - sizeof(endMark));
 
     // Compute the number of lines we can write
     todo = lines<todo? lines : todo;
