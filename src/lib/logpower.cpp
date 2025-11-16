b3160e4da96370126e36547da53b506277b26152
Marat Fayzullin <luarvique@gmail.com>
2025-11-15 21:57:49 -0500

    Trying alternative logpower implementation.

--- src/lib/logpower.cpp
+++ src/lib/logpower.cpp
@@ -39,6 +39,7 @@ LogPower::LogPower(float add_db): add_db(add_db) {}
 
 LogPower::LogPower(): LogPower(0.0) {}
 
+#if 1
 void LogPower::process(complex<float>* input, float* output, size_t size) {
     for (int i = 0; i < size; i++) {
         output[i] = std::norm(input[i]);
@@ -52,3 +53,11 @@ void LogPower::process(complex<float>* input, float* output, size_t size) {
         output[i] = 10.0f * output[i] + add_db;
     }
 }
+#else
+void LogPower::process(complex<float>* input, float* output, size_t size) {
+    for (int i = 0; i < size; i++) {
+        output[i] = 10.0f * std::log10(std::norm(input[i])) + add_db;
+    }
+}
+#endif
+
