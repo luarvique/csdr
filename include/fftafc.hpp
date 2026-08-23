3f38722d9b6262968568626f3a5ad921f23b53ad
Marat Fayzullin <luarvique@gmail.com>
2026-08-23 19:29:28 -0400

    Renaming AFC modules.

---
+++ include/fftafc.hpp
@@ -0,0 +1,52 @@
+/*
+Copyright (c) 2023 Marat Fayzullin <luarvique@gmail.com>
+
+This file is part of libcsdr.
+
+libcsdr is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+libcsdr is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libcsdr.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "module.hpp"
+#include "shift.hpp"
+
+#include <fftw3.h>
+
+namespace Csdr {
+
+    class FftAfc: public ShiftAddfast {
+        public:
+            FftAfc(unsigned int updatePeriod = 4, unsigned int samplePeriod = 1);
+            ~FftAfc();
+
+        protected:
+            void process(complex<float>* input, complex<float>* output) override;
+
+        private:
+            // Configuration
+            unsigned int updatePeriod; // Update period
+            unsigned int samplePeriod; // Sampling period (<=updatePeriod)
+
+            // State
+            unsigned int updateCount;  // Update counter
+            double curShift;           // Current frequency correction
+
+            // FFT setup
+            fftwf_complex *fftIn;
+            fftwf_complex *fftOut;
+            fftwf_plan fftPlan;
+            float *inputWindow;
+    };
+}
--- src/lib/afc.cpp
+++ src/lib/afc.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2023 Marat Fayzullin <luarvique@gmail.com>
+Copyright (c) 2026 Marat Fayzullin <luarvique@gmail.com>
 
 This file is part of libcsdr.
 
@@ -18,109 +18,73 @@ along with libcsdr.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "afc.hpp"
-#include "complex.hpp"
-#include <cstring>
-#include <cstdlib>
+
 #include <cmath>
 
 using namespace Csdr;
 
-#if defined __arm__ || __aarch64__
-#define CSDR_FFTW_FLAGS (FFTW_DESTROY_INPUT | FFTW_ESTIMATE)
-#else
-#define CSDR_FFTW_FLAGS (FFTW_DESTROY_INPUT | FFTW_MEASURE)
-#endif
+Afc::Afc(float sampleRate, float bandwidthHz, float dampingFactor)
+: sampleRate(sampleRate)
+{
+    // Standard 2nd-order PLL loop filter design (bilinear approximation).
+    // See e.g. Gardner, "Phaselock Techniques"; the same formulas are used
+    // in liquid-dsp's nco_crcf_pll and similar libraries.
+    float bnT = bandwidthHz / sampleRate;
+    float theta = bnT / (dampingFactor + 1.0f / (4.0f * dampingFactor));
+    float d = 1.0f + 2.0f * dampingFactor * theta + theta * theta;
 
-// Hann window function
-static inline float hann(unsigned int x, unsigned int size) {
-    return 0.5f - 0.5f * std::cos((2.0 * M_PI * x) / size);
+    alpha = 4.0f * dampingFactor * theta / d;  // proportional gain
+    beta  = 4.0f * theta * theta / d;          // integral gain
 }
 
-// Squared magnitude of a complex value
-static float mag2(const fftwf_complex &v)
+float Afc::getFrequency() const
 {
-    return v[0] * v[0] + v[1] * v[1];
-}
-
-Afc::Afc(unsigned int updatePeriod, unsigned int samplePeriod): ShiftAddfast(0.0)
-{
-    // Verify and initialize configuration
-    this->samplePeriod = samplePeriod = samplePeriod>1? samplePeriod : 1;
-    this->updatePeriod = updatePeriod = updatePeriod>samplePeriod? updatePeriod : samplePeriod;
-    updateCount = updatePeriod;
-    curShift    = 0.0;
-
-    // Set up FFT
-    unsigned int fftSize = samplePeriod * getLength();
-    fftIn   = fftwf_alloc_complex(fftSize);
-    fftOut  = fftwf_alloc_complex(fftSize);
-    fftPlan = fftwf_plan_dft_1d(fftSize, fftIn, fftOut, FFTW_FORWARD, CSDR_FFTW_FLAGS);
-
-    // Precompute input window
-    inputWindow = new float[fftSize];
-    for(size_t i=0; i < fftSize; i++)
-        inputWindow[i] = hann(i, fftSize);
-}
-
-Afc::~Afc()
-{
-    // Destroy FFT
-    fftwf_destroy_plan(fftPlan);
-    fftwf_free(fftIn);
-    fftwf_free(fftOut);
-    delete [] inputWindow;
+    return freq * sampleRate / (2.0f * (float)M_PI);
 }
 
 void Afc::process(complex<float>* input, complex<float>* output)
 {
-    unsigned int size = getLength();
-    int j, i;
+    const size_t size = getLength();
 
-    // Count updates
-    updateCount--;
-
-    // If sampling input signal...
-    if(updateCount < samplePeriod)
+    for(size_t n = 0; n < size; ++n)
     {
-        // Copy input signal into the buffer
-        i = (samplePeriod - updateCount - 1) * size;
-        for(j=0 ; j<size ; ++j, ++i)
+        // Mix the input down by the current NCO phase estimate, i.e.
+        // multiply by e^{-j*phase} to cancel a carrier sitting at +phase.
+        float c = cosf(phase), s = sinf(phase);
+        float yi =  input[n].real() * c + input[n].imag() * s;
+        float yq = -input[n].real() * s + input[n].imag() * c;
+
+        output[n] = complex<float>(yi, yq);
+
+        // Running magnitude estimate (simple one-pole smoother), used to
+        // gate the phase detector below.
+        float mag = sqrtf(yi * yi + yq * yq);
+        magAvg += 0.001f * (mag - magAvg);
+
+        // Phase detector: the angle of the corrected sample is the
+        // instantaneous phase error. This is amplitude-independent, so
+        // AM's own envelope modulation doesn't bias it directly -- except
+        // very close to modulation nulls, where magnitude (and therefore
+        // phase information) is dominated by noise; we skip the loop
+        // update there rather than feed it garbage.
+        if(mag > 0.3f * magAvg)
         {
-            std::complex<float> v = input[j] * inputWindow[i];
-            fftIn[i][0] = v.real();
-            fftIn[i][1] = v.imag();
+            float error = atan2f(yq, yi);
+
+            // PI loop filter: integral term tracks frequency (persistent
+            // drift), proportional term reacts to instantaneous error.
+            freq  += beta  * error;
+            phase += alpha * error;
+
+            // Smoothed lock-quality indicator, ~1.0 when phase error is
+            // small and consistent.
+            lockIndicator += 0.001f * (cosf(error) - lockIndicator);
         }
 
-        // If detecting the carrier...
-        if(!updateCount)
-        {
-            // Reset update counter
-            updateCount = updatePeriod;
-
-            // Calculate FFT on the input buffer
-            fftwf_execute(fftPlan);
-
-            unsigned int fftSize = size * samplePeriod;
-            float maxMag = mag2(fftOut[0]);
-
-            // Find the carrier frequency
-            for(j=1, i=0 ; j<fftSize ; ++j)
-            {
-                float mag = mag2(fftOut[j]);
-                if(mag>maxMag) { i=j;maxMag=mag; }
-            }
-
-            // Take negative shifts into account
-            i = i>=fftSize/2? fftSize-i : -i;
-
-            // Update frequency shift, if the change is large enough
-            double newShift = (double)i / fftSize;
-            double minShift = 1.0 / fftSize;
-            if(std::abs(newShift-curShift) >= minShift)
-                setRate(curShift = newShift);
-        }
+        // Advance the NCO by the current frequency estimate every sample,
+        // regardless of whether the detector fired this sample.
+        phase += freq;
+        while(phase >  (float)M_PI) phase -= 2.0f * (float)M_PI;
+        while(phase < -(float)M_PI) phase += 2.0f * (float)M_PI;
     }
-
-    // Shift frequency
-    process_fmv(input, output, size);
 }
